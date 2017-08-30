179e80f9800d6fa5a189a04d2f802cbf5f72aa26
AlexV <asmodehn@gmail.com>
2017-08-30 20:32:12 +0900

    first basic types

---
+++ pycraft/__main__.py
@@ -0,0 +1,16 @@
+
+import yaml
+import argparse
+
+
+def parse(filename):
+
+    with open(filename, 'r') as f:
+        ast = yaml.safe_load(f)
+
+    print(ast)
+
+
+if __name__ == '__main__':
+    parse("examples/ground_types.yaml")
+
